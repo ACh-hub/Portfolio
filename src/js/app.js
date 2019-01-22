6d0275009d295a7c61baa1a2eae08bb5a220c43c
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-01-22 16:28:50 +0100

    css refactor

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,12 @@
-// When the user scrolls the page, execute myFunction 
 window.onscroll = function() {toggleSticky()};
 
-// Get the navbar
 const navbar = document.querySelector(".main-header");
-
-// Get the offset position of the navbar
 const sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function toggleSticky() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
 }
