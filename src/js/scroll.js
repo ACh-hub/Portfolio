62dc9a170ba9839fc3039d4b179474cbebc7ff96
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-01-23 14:09:56 +0100

    highlight done, pretty hacky. smooth scrolling works for chrome, no workaround for dinosaurs

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,13 +1,33 @@
 const navbar = document.querySelector(".main-header");
 const sticky = navbar.offsetTop;
-
 const spaiedDivs = Array.from(document.querySelectorAll(".content"));
+const contactDiv = document.getElementById("section-contact");
+const portfolioDiv = document.getElementById("section-portfolio");
+
+window.onload= function () {
+    contactDiv.addEventListener("mouseenter", toggleActive);
+    portfolioDiv.addEventListener("mouseenter", toggleActive);
+    };
+
+    
+    function toggleActive(e) {
+        spaiedDivs.forEach(spaiedDiv=>{
+            let spaiedLink = document.querySelector(`a[href*=${spaiedDiv.id}]`);
+            if(spaiedDiv.id != e.target.id){
+                spaiedLink.classList.remove("active");
+            }
+            else
+            {
+                spaiedLink.classList.add("active");
+            }
+        })
+        
+    }
 
 window.onscroll = function () {
     toggleSticky();
     spaiedDivs.forEach(spaiedDiv=>{
         let spaiedLink = document.querySelector(`a[href*=${spaiedDiv.id}]`)
-        console.log(spaiedLink);
         
         if(checkVisible(spaiedDiv))
         {
@@ -18,13 +38,12 @@ window.onscroll = function () {
             spaiedLink.classList.remove("active");
         }
     })
-
 };
+
 
 function checkVisible(el) {
     var rect = el.getBoundingClientRect();
-    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
-    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+    return !(rect.bottom < 0 || rect.top >= 0);
 }
 
 function toggleSticky() {
