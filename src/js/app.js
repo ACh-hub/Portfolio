82437f003d611a24246ca34141a11721edb8dce4
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-01-30 19:18:11 +0100

    got rid of hamburger menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,5 @@
-import MobileNavBar from "./modules/MobileNavBar";
 import StickyNavBar from "./modules/StickyNavBar";
 
 "use strict";
 
-const mobileNavBar = new MobileNavBar();
 const stickyNavBar = new StickyNavBar();
