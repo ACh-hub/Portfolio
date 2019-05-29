f4e5ffa055a272ab2ee864b31991892f87024fb9
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-05-29 23:10:44 +0200

    added sticky

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,23 @@ import "regenerator-runtime/runtime";
 
 import "../scss/styles.scss";
 
-
-import StickyNavBar from "./modules/StickyNavBar";
+import Sticky from "../js/modules/sticky";
 
 "use strict";
 
-const stickyNavBar = new StickyNavBar();
+const stickyHeader = new Sticky();
+
+window.onscroll = function (e) {
+    let offset = window.pageYOffset;
+    let distance = stickyHeader.getDistance() - offset;
+
+    if ((distance <= 0) && !stickyHeader.stuck) {
+        stickyHeader.element.style.position = 'fixed';
+        stickyHeader.element.style.top = '0px';
+        stickyHeader.stuck = true;
+    } else if (stickyHeader.stuck && (offset <= stickyHeader.getDistance())) {
+        stickyHeader.element.style.position = 'static';
+        stickyHeader.stuck = false;
+    }
+
+}
---
+++ src/js/modules/sticky.js
@@ -0,0 +1,11 @@
+export default class Sticky {
+    constructor(){
+        this.element = document.querySelector('.main-nav');
+        this.stuck = false;
+    }
+
+    getDistance(){
+        return this.element.offsetTop;
+    }
+
+}
