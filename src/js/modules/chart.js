b5f0d04bb5819ccda55709de959a13e4d1dcd048
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-06-01 16:25:18 +0200

    json done

--- src/js/modules/chart.js
+++ src/js/modules/chart.js
@@ -2,212 +2,79 @@ import * as d3 from 'd3';
 
 export default class Chart {
     constructor(chartContainer, svgId) {
-        this.chartContainer = chartContainer;
-        this.svgId = svgId;
-
         /////// Chart magic numbers /////////
         this.chargeStrength = 50;
         this.collisionStrength = 16;
-    }
 
-
-    init() {
-
-        this.height = this.chartContainer.offsetHeight;
+        this.chartContainer = chartContainer;
+        this.svgId = svgId;
         this.width = this.chartContainer.offsetWidth;
+        this.height = this.chartContainer.offsetHeight;
 
-        this.chartData = {
-            "nodes": [{
-                    "skill": "Javascript (ES6)",
-                    "class": "dev",
-                    "level": 4
-                },
-                {
-                    "skill": "CSS3",
-                    "class": "dev",
-                    "level": 4
-                },
-                {
-                    "skill": "Sass",
-                    "class": "dev",
-                    "level": 4
-                },
-                {
-                    "skill": "Bootstrap",
-                    "class": "dev",
-                    "level": 1
-                },
-                {
-                    "skill": "Git",
-                    "class": "dev",
-                    "level": 4
-                },
-                {
-                    "skill": "HTML5",
-                    "class": "dev",
-                    "level": 5
-                },
-                {
-                    "skill": "Webpack",
-                    "class": "dev",
-                    "level": 1
-                },
-                {
-                    "skill": "Babel",
-                    "class": "dev",
-                    "level": 1
-                },
-                {
-                    "skill": "Gulp",
-                    "class": "dev",
-                    "level": 1
-                },
-                {
-                    "skill": "REST",
-                    "class": "dev",
-                    "level": 1
-                },
-                {
-                    "skill": "d3.js",
-                    "class": "dev",
-                    "level": 3
-                },
-                {
-                    "skill": "C++",
-                    "class": "dev",
-                    "level": 3
-                },
-                {
-                    "skill": "Java",
-                    "class": "dev",
-                    "level": 1
-                },
-                {
-                    "skill": "Photoshop",
-                    "class": "des",
-                    "level": 3
-                },
-                {
-                    "skill": "RWD",
-                    "class": "des",
-                    "level": 1
-                },
-                {
-                    "skill": "Figma",
-                    "class": "des",
-                    "level": 1
-                },
-                {
-                    "skill": "Polish",
-                    "class": "lang",
-                    "level": 5
-                },
-                {
-                    "skill": "English",
-                    "class": "lang",
-                    "level": 4
-                },
-                {
-                    "skill": "French",
-                    "class": "lang",
-                    "level": 1
-                },
-                {
-                    "skill": "Russian",
-                    "class": "lang",
-                    "level": 2
-                },
-            ]
-        };
-
-        // chart svg
+        // svg add responsivity
         const svg = d3.select(this.chartContainer)
             .append("svg")
-            .attr('id', this.svgId)
-            .attr('width', this.width)
-            .attr('height', this.height)
+            .attr("width", this.width)
+            .attr("height", this.height)
             .call(responsivefy);
 
-            function responsivefy(svg) {
-                var container = d3.select(svg.node().parentNode),
-                    width = parseInt(svg.style("width")),
-                    height = parseInt(svg.style("height")),
-                    aspect = width / height;
+        function responsivefy(svg) {
+            let container = d3.select(svg.node().parentNode);
+            let width = parseInt(svg.style("width"));
+            let height = parseInt(svg.style("height"));
+            let aspect = width / height;
 
-                svg.attr("viewBox", "0 0 " + width + " " + height)
-                    .attr("perserveAspectRatio", "xMinYMid")
-                    .call(resize);
-            
-                d3.select(window).on("resize." + container.attr("id"), resize);
-            
-                function resize() {
-                    var targetWidth = parseInt(container.style("width"));
-                    svg.attr("width", targetWidth);
-                    svg.attr("height", Math.round(targetWidth / aspect));
-                }
+            svg.attr("viewBox", "0 0 " + width + " " + height)
+                .attr("perserveAspectRatio", "xMinYMid")
+                .call(resize);
+
+            d3.select(window).on("resize." + container.attr("id"), resize);
+
+            function resize() {
+                var targetWidth = parseInt(container.style("width"));
+                svg.attr("width", targetWidth);
+                svg.attr("height", Math.round(targetWidth / aspect));
             }
+        }
 
+        this.svg = svg;
 
-        // definitions for patterns
+        // definitions of patterns
         const defs = svg.append("defs");
         const pattern = defs.append("pattern")
-        .attr("id","stripes-1")
-        .attr("patternUnits","userSpaceOnUse")
-        .attr("width",10)
-        .attr("height",10);
+            .attr("id", "stripes-1")
+            .attr("patternUnits", "userSpaceOnUse")
+            .attr("width", 10)
+            .attr("height", 10);
         const image = pattern.append("image")
-        .attr("xlink:href","data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI3MCIgaGVpZ2h0PSI3MCI+CjxyZWN0IHdpZHRoPSI3MCIgaGVpZ2h0PSI3MCIgZmlsbD0iI2JiZDgxNyI+PC9yZWN0Pgo8ZyB0cmFuc2Zvcm09InJvdGF0ZSg0NSkiPgo8cmVjdCB3aWR0aD0iOTkiIGhlaWdodD0iMjUiIGZpbGw9IiNhOWNlMDAiPjwvcmVjdD4KPHJlY3QgeT0iLTUwIiB3aWR0aD0iOTkiIGhlaWdodD0iMjUiIGZpbGw9IiNhOWNlMDAiPjwvcmVjdD4KPC9nPgo8L3N2Zz4=")
-        .attr("x",0)
-        .attr("y",0)
-        .attr("width",10)
-        .attr("height",10);
+            .attr("xlink:href", "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI3MCIgaGVpZ2h0PSI3MCI+CjxyZWN0IHdpZHRoPSI3MCIgaGVpZ2h0PSI3MCIgZmlsbD0iI2JiZDgxNyI+PC9yZWN0Pgo8ZyB0cmFuc2Zvcm09InJvdGF0ZSg0NSkiPgo8cmVjdCB3aWR0aD0iOTkiIGhlaWdodD0iMjUiIGZpbGw9IiNhOWNlMDAiPjwvcmVjdD4KPHJlY3QgeT0iLTUwIiB3aWR0aD0iOTkiIGhlaWdodD0iMjUiIGZpbGw9IiNhOWNlMDAiPjwvcmVjdD4KPC9nPgo8L3N2Zz4=")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", 10)
+            .attr("height", 10);
+        this.defs = defs;
 
-
-        // Holds child components (nodes, links), i.e. all but the background
-        const svgGroup = svg
-            .append('svg:g')
-            .attr("id", "svgGroup");
-        this.svgGroup = svgGroup;
-
-        // Holds nodes
+        // group for the circles
         const graphNodesGroup =
-            svgGroup
+            this.svg
             .append("g")
             .attr("id", `nodes_${this.svgId}`)
             .attr("class", "nodes");
         this.graphNodesGroup = graphNodesGroup;
-
-        // Simulation
-        const simulation = this.initSimulation();
-        this.simulation = simulation;
-
-        // update();
-        this.update(this, simulation);
-
     }
 
-    initSimulation() {
-  // Features of the forces applied to the nodes:
+    async init() {
 
+        //retrieve data for nodes
+        const data = await d3.json("../data.json");
+        this.chartData = data;
 
-        let result = d3.forceSimulation()
-            .force('center', d3.forceCenter(this.width / 2, this.height/1.8))
-            .force("charge", d3.forceManyBody().strength(this.chargeStrength))
-            .force('collision', d3.forceCollide().radius(d=>{
-                return d.level*this.collisionStrength;
-            }))
-        return result;
-
-    }
-
-    update(t, simulation) {
-        const nodes = t.chartData.nodes;
-
-        // nodes
-        const graphNodeCircles = t.graphNodesGroup.selectAll("circle")
-            .data(nodes)
+        // create groups and bind data
+        const graphNodeCircles = this.graphNodesGroup.selectAll("circle")
+            .data(data.nodes)
             .enter().append("g");
 
+        // create and append circles
         const circles = graphNodeCircles.append("circle")
             .attr("r", d => {
                 return d.level * this.collisionStrength - d.level;
@@ -232,30 +99,51 @@ export default class Chart {
             .attr("x", 0)
             .attr("y", 4)
             .attr("text-anchor", "middle")
-            .style("font-size",d=>{
-                switch(d.level){
-                    case 1:                 
+            .style("font-size", d => {
+                switch (d.level) {
+                    case 1:
                         return "0.6em";
                     case 2:
                         return "0.7em";
                     default:
                         return "0.8em";
-                
+
                 }
             });
 
-        //apply simulation
+        this.graphNodeCircles = graphNodeCircles;
+
+
+        // Simulation
+        const simulation = this.initSimulation();
+        this.simulation = simulation;
+
+        // update();
+        this.update(this, simulation);
+    }
+
+    initSimulation() {
+        let result = d3.forceSimulation()
+            .force('center', d3.forceCenter(this.width / 2, this.height / 1.8))
+            .force("charge", d3.forceManyBody().strength(this.chargeStrength))
+            .force('collision', d3.forceCollide().radius(d => {
+                return d.level * this.collisionStrength;
+            }))
+        return result;
+    }
+
+    update(t, simulation) {
+        const nodes = t.chartData.nodes;
+
         simulation
             .nodes(nodes)
             .on("tick", handleTicked)
 
         function handleTicked() {
-            graphNodeCircles.
+            t.graphNodeCircles.
             attr("transform", d => {
                 return `translate(${[d.x, d.y]})`;
             })
         }
-
     }
 }
-
